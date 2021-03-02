f78394a97e33d5e4da06041c3c91142184338973
D0KKAEl3I <dldudtjr850@gmail.com>
2021-03-02 09:11:13 +0900

    .

--- src/layouts/Index/index.js
+++ src/layouts/Index/index.js
@@ -4,53 +4,63 @@ import Slot from 'components/Slot'
 import styles from './index.module.css'
 
 export default function Index() {
-    const [slots, setSlots] = useState([])
-    useEffect(() => {
-        let arr = []
+    const [loading, setLoading] = useState(false)
 
+    const [slots, setSlots] = useState([])
+
+    useEffect(() => {
+        setLoading(true)
+        let arr = [];
+        let remainRandomNumber = 11;
         for (let y = 0; y < 9; y++) {
             let row = []
             for (let x = 0; x < 9; x++) {
-                row.push({ number: null, checked: [], point: { x: x, y: y }, select: false, correct: true })
+                row.push({ number: null, checked: [], point: { x: x, y: y }, select: false, correct: false })
             }
             arr.push(row)
         }
         setSlots(arr)
+        setLoading(false)
     }, [])
 
     useEffect(() => {
-        if (selectedSlotPoint.x && selectedSlotPoint.y) {
-            let checked = check(selectedSlotPoint)
-            setSlots(arr => {
-                let res = [...arr]
-                res[selectedSlotPoint.y][selectedSlotPoint.y].checked = checked
-                return res
-            })
+        if (!setLoading) {
+            for (let y = 0; y < slots.length; y++) {
+                for (let x = 0; x < slots[y].length; x++) {
+                    console.log(slots[y][x].correct)
+                    if (!slots[y][x].correct) return
+                }
+            }
+            alert('congratulations')
         }
     }, [slots])
-
-    const check = point => {
-        if (point.x && point.y) {
-            let number = slots[point.y][point.x].number
-            //row
-            for (let i in slots[point.y]) {
-                if (slots[point.y][i].point != point && slots[point.y][i].number == number) return false
-            }
-            //column
-            for (let i in slots) {
-                if (slots[i][point.x].point != point && slots[i][point.x].number == number) return false
-            }
-
-            //block
-
-            return true
-        }
-    }
 
     const [selectedSlotPoint, setSelectedSlotPoint] = useState({ x: null, y: null })
 
     const select = (point) => {
         setSelectedSlotPoint(point)
+    }
+
+    const check = (point, number) => {
+        if (point.x && point.y) {
+            let checked = true;
+            //cross check
+            for (let i = 0; i < slots.length; i++) {
+                //row
+                if (slots[point.y][i].point != point && slots[point.y][i].number == number) checked = false
+                //column
+                else if (slots[i][point.x].point != point && slots[i][point.x].number == number) checked = false
+            }
+            //block check
+            let Ygap = point.y - point.y % 3
+            for (let y = Ygap; y < Ygap + 3; y++) {
+                let Xgap = point.x - point.x % 3
+                for (let x = Xgap; x < Xgap + 3; x++) {
+                    if (slots[y][x].number == number && slots[y][x].point != point) checked = false
+                }
+            }
+            return checked
+        }
     }
 
     useEffect(() => {
@@ -63,19 +73,22 @@ export default function Index() {
                 }
             }
             //set
-            if (selectedSlotPoint.x && selectedSlotPoint.y) {
-                arr[selectedSlotPoint.y][selectedSlotPoint.x].select = true
+            if (typeof selectedSlotPoint.x == 'number' && typeof selectedSlotPoint.y == 'number') {
+                if (0 <= selectedSlotPoint.x <= 8 && 0 <= selectedSlotPoint.y <= 8) {
+                    arr[selectedSlotPoint.y][selectedSlotPoint.x].select = true
+                }
             } return arr
         })
     }, [selectedSlotPoint])
 
     const setNumber = (number) => {
-        if (selectedSlotPoint.x && selectedSlotPoint.y) {
-            let { x, y } = selectedSlotPoint
+        let { x, y } = selectedSlotPoint
+        if (typeof x == 'number' && typeof y == 'number') {
             setSlots(arr => {
                 arr = [...arr]
                 arr[y][x].number = number
-                return arr;
+                arr[y][x].correct = check(selectedSlotPoint, number, slots)
+                return arr
             })
         }
     }
@@ -83,16 +96,24 @@ export default function Index() {
     const [remainNums, setRemainNums] = useState([9, 9, 9, 9, 9, 9, 9, 9, 9])
 
     return (
-        <div onClick={e => {
-            console.log(e.currentTarget == e.target)
-            if (e.currentTarget == e.target) setSelectedSlotPoint({ x: null, y: null })
-        }}>
-            <div className={styles.slotContainer}>
-                {
-                    slots.map(row => row.map(item => <Slot select={item.select} number={item.number} checked={item.checked} point={item.point} functions={{ select }} />))
-                }
-            </div>
-            <NumberSelecter functions={{ setNumber }} style={{ position: 'absolute', bottom: '40px', left: '50%', transform: 'translateX(-50%)' }} remainNums={remainNums} />
+        <div>
+            {
+                loading ?
+                    ''
+                    :
+                    <div onClick={e => {
+                        if (e.currentTarget == e.target) setSelectedSlotPoint({ x: null, y: null })
+                    }}>
+                        <div className={styles.slotContainer}>
+                            {
+                                slots.map(row => row.map(item => <Slot select={item.select} number={item.number} checked={item.checked} point={item.point} functions={{ select }} />))
+                            }
+                        </div>
+                        <NumberSelecter functions={{ setNumber }} style={{ position: 'absolute', bottom: '40px', left: '50%', transform: 'translateX(-50%)' }} remainNums={remainNums} />
+                    </div>
+            }
         </div>
+
+
     )
 }
